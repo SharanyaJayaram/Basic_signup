ede43bcc853ffc64831405424274352091726bef
Anusha <anushamns@gmail.com>
2023-03-10 14:41:12 +0530

    added auto response for username and email

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-from django.contrib import messages
-from django.core.mail import EmailMessage, send_mail
-from django.contrib.auth import authenticate, login, logout
 import re
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 
 
 def home(request):
@@ -28,18 +29,19 @@ def signup(request):
         #     messages.error(request, "Email Already Registered!!")
         #     return redirect('signin')
 
-        try:
-            if User.objects.get(username=username):
-                messages.info(request, "UserName Is Taken")
-                return redirect('signup')
-        except:
-            pass
-        try:
-            if User.objects.get(email=email):
-                messages.info(request, "Email Is Taken")
-                return redirect('signup')
-        except:
-            pass
+        # try:
+        #     if User.objects.get(username=username):
+        #         messages.info(request, "UserName Is Taken")
+        #         return redirect('signup')
+        # except:
+        #     pass
+        if get_user_model().objects.filter(username=username).exists():
+            return HttpResponse('<div style="color: red"> This username already exists </div>')
+        elif get_user_model().objects.filter(email=email).exists():
+            return HttpResponse('<div style="color: red"> This email id already exists </div>')
+        else:
+            return HttpResponse('<div style="color: green"> Available </div>')
+
 
         if len(username) < 8:
             messages.error(request, "Username must be of minimum 8 characters!!")
@@ -48,6 +50,8 @@ def signup(request):
         if username.isalpha():
             messages.error(request, "Username must be Alpha-Numeric!!")
             return redirect('signup')
+
+
 
         def validate_password(password):
             # regex = re.compile(r'[@_!#$%^&*()<>?/\|}{~:]')
@@ -64,7 +68,7 @@ def signup(request):
                 messages.error(request, "Password should include atleast 1 number")
                 return True
             if not re.search("[^a-zA-Z0-9]", password):
-                messages.error(request, "Password should include atleast 1 number")
+                messages.error(request, "Password should include atleast 1 special character")
                 return True
             # if not regex.search(password) == None:
             #     messages.error(request, "Password should include atleast 1 special character")
@@ -81,7 +85,6 @@ def signup(request):
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
-        #myuser.is_active = False
         myuser.save()
         messages.success(request, "Your Account has been created succesfully!! ")
 
