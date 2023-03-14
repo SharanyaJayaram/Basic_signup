645ae1e1764250f0862bc684b0916fb631408a3d
priya <116056607+chaitanyapriya123@users.noreply.github.com>
2023-03-14 22:43:20 +0530

    Delete branchnaming.sh

--- branchnaming.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-branch_name=$(git symbolic-ref --short HEAD)
-
-if [[ "$branch_name" =~ ^[a-z] ]]; then
-  echo "Error: Branch name should start with an uppercase letter."
-  exit 1
-fi
